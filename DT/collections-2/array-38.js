25b89da82ccec9630a0dbc6f54f114713fc96ded
Alexander86528 <74484953+Alexander86528@users.noreply.github.com>
2022-05-03 22:11:54 +0700

    update array-38.js

---
+++ DT/collections-2/array-38.js
@@ -0,0 +1,62 @@
+
+console.log("task 1");
+let arr = ['a', 'b', 'c'];
+alert(arr);
+console.log(arr);
+/*
+console.log('task 2');
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+console.log('task 3');
+arr [3] = 'd';
+alert(arr);
+console.log(arr[0] + '+' + arr[1] + '+' + arr[2] + '+' + arr[3]);
+res = arr[0] + '+' + arr[1] + '+' + arr[2] + '+' + arr[3];
+console.log(res);
+
+console.log('task 4');
+arr = [1, 2, 3];
+console.log(1 + 2 + 3);
+
+console.log('task 5');
+arr = [2, 5, 3, 9];
+res = arr[0] * arr[1] + arr[2] * arr[3];
+console.log(res);
+
+console.log('task 6');
+arr = [1, 2, 3, 4];
+alert(arr.length);
+console.log(arr.length);
+
+console.log('task 7');
+console.log(arr[arr.length - 1]);
+
+console.log('task 8');
+arr[0] = 1;
+arr[1] = 3;
+arr[3] = -2;
+*/
+console.log('tadk 9');
+arr = [1, 2, 3];
+arr[0] += 3;
+arr[1] += 3;
+arr[2] += 3;
+console.log(arr);
+
+console.log('task 10');
+arr[0]++;
+arr[1]++;
+arr[2]++;
+console.log(arr);
+
+arr[3] = 4;
+arr[4] = 5;
+console.log(arr);
+
+console.log('task 13');
+let arr2 = [];
+arr2[3] = 'a';
+arr2[8] = 'b';
+console.log(arr2.length);
