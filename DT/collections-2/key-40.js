cbb989f2f63fc70b4ab084c508f6122bb0f3b2c2
Alexander86528 <74484953+Alexander86528@users.noreply.github.com>
2022-05-04 20:47:39 +0700

    update key-40.js

--- DT/collections-2/key-40.js
+++ DT/collections-2/key-40.js
@@ -1,1 +1,13 @@
 
+console.log('task 1');
+let arr = ['a', 'b', 'c'];
+let key = 2;
+console.log(arr);
+console.log(arr[key]);
+
+console.log('task 2');
+arr2 = [1, 2, 3, 4, 5];
+let key1 = 1;
+let key2 = 2;
+console.log(arr2);
+console.log(arr2[key1] + arr2[key2]);
