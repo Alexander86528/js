f04d53f9c07810dcca2fcf46a9b2ef63c58683e0
Alexander86528 <dl800259@gmail.com>
2022-05-09 07:10:24 +0700

    tasks 63-66

---
+++ DT/cycles-4/js/while-63.js
@@ -0,0 +1,42 @@
+console.log('example 63');
+let i = 1;
+
+while(i <= 5) {
+  console.log(i);
+  i++;
+}
+
+console.log('task 63-1');
+i = 1;
+while(i <= 100) {
+  console.log(i);
+  i++;
+}
+
+console.log('task 63-2');
+i = 11;
+while(i <= 33) {
+  console.log(i);
+  i++;
+}
+
+console.log('task 63-3');
+i = 0;
+while(i <= 100) {
+  console.log(i);
+  i += 2;
+}
+
+console.log('task 63-4');
+i = 1;
+while(i <= 99) {
+  console.log(i);
+  i += 2;
+}
+
+console.log('task 63-5');
+i = 30;
+while(i >= 0) {
+  console.log(i);
+  i--;
+}
