138569fb17de3ec31dda3a2597597dbc788f1aac
Alexander86528 <74484953+Alexander86528@users.noreply.github.com>
2022-05-12 06:22:44 +0700

    update const-array-45.js

--- DT/collections-2/const-array-45.js
+++ DT/collections-2/const-array-45.js
@@ -1,1 +1,10 @@
+const arr1 = ['a', 'b', 'c'];
+console.log(arr1[0]);
+arr1[0] = '!';
+console.log(arr1[0]);
+console.log(arr1);
 
+const arr2 = ['c', 'd', 'e'];
+// arr2 = [1, 2, 3];
+// arr2 = ['c', 'd', 'e'];
+console.log(arr2);
