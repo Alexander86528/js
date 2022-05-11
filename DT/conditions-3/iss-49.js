2b66752adeef646a0991223d005b63f4da3bfc98
Alexander86528 <74484953+Alexander86528@users.noreply.github.com>
2022-05-11 20:52:53 +0700

    update iss-49.js

--- DT/conditions-3/iss-49.js
+++ DT/conditions-3/iss-49.js
@@ -1,1 +1,24 @@
+let test = 'abc';
+if (test == 'abc') {
+  console.log('true');
+} else {
+  console.log('false');
+}
 
+if ('3' == 3) {
+  console.log('true');
+} else {
+  console.log('false');
+}
+
+if ('3' != 3) {
+  console.log('true');
+} else {
+  console.log('false');
+}
+
+if (0 == ' ') {
+  console.log('true');
+} else {
+  console.log('false');
+}
