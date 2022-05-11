151242dc69f5cc8b97fdbc5b6c403e91491af5af
Alexander86528 <74484953+Alexander86528@users.noreply.github.com>
2022-05-12 04:40:41 +0700

    update field-of-view-55.js

--- DT/conditions-3/field-of-view-55.js
+++ DT/conditions-3/field-of-view-55.js
@@ -1,1 +1,26 @@
+let res = 1;
 
+if (true) {
+  let res = 2;
+  console.log(res);
+}
+console.log(res);
+
+let age = 17;
+let abult;
+
+if (age >= 18) {
+  abult = true;
+} else {
+  abult = false;
+}
+console.log(abult);
+
+let num = 2;
+
+if (num == 1) {
+  var result = 'true';
+} else {
+  var result = 'false';
+}
+console.log(result);
