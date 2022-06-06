68a637849d6411cafa6f2d40e9e364d810d51341
Alexander86528 <74484953+Alexander86528@users.noreply.github.com>
2022-06-06 07:04:24 +0700

    update break-72.js

--- DT/cycles-4/js/break-72.js
+++ DT/cycles-4/js/break-72.js
@@ -1,1 +1,26 @@
-
+/*
+let arr = [0, 12, 3, 47, 1];
+console.log(arr);
+for (let el of arr) {
+  if (el != 1) {
+    console.log(el);
+  } else {
+    console.log('yes');
+    break;
+  }
+}
+*/
+/*
+let arr2 = [1, 2, 3, 4, -3];
+let sum = 0;
+console.log(arr2);
+for (elem2 of arr2) {
+  if (elem2 > 0) {
+    sum += elem2;
+  } else {
+    // console.log('elem2 < 0');
+    break;
+  }
+}
+console.log(sum);
+*/
