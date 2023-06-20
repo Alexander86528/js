e4d619a1d8d5e6dfedd56730306c74da938e05f2
kub060522 <sg01.04.21m31@gmail.com>
2023-06-20 20:14:33 +0700

    add task 201

---
+++ DT/functions-8/js/f201.js
@@ -0,0 +1,15 @@
+console.log('task 200 1');
+function foo1 (num) {
+  return num ** 3;
+}
+console.log(foo1(2));
+
+console.log('task 200 2');
+function foo2(num2) {
+  if (num2 > 0) {
+    console.log('+++');
+  } else {
+    console.log('---');
+  }
+}
+foo2(-1);
