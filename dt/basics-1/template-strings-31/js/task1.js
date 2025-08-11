beab6d9514a3e1935fa0b194e5643d1c844278d7
kub060522 <sg01.04.21m31@gmail.com>
2025-08-11 08:34:24 +0700

    Добавляет файлы "template strings 31".
    И удаляет ненужные файлы.

---
+++ dt/basics-1/template-strings-31/js/task1.js
@@ -0,0 +1,5 @@
+let str5 = 'xxx';
+let str6 = 'yyy';
+
+let str7 = `aaa ${str5} bbb ${str6} ccc`;
+document.writeln(str7);
---
+++ dt/basics-1/template-strings-31/js/template-strings.js
@@ -0,0 +1,13 @@
+let str1 = `bbb`;
+//alert(str1);
+document.writeln(str1);
+document.writeln('<br>');
+let str2 = 'aaa ' + str1 + ' ccc';
+// alert(str2);
+document.writeln(str2);
+document.writeln('<br>');
+
+let str3 = 'ddd';
+let str4 = `ccc ${str3} eee`;
+document.writeln(str4);
+document.writeln('<br>');
